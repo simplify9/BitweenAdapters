ea0c9c6b73a78515d84b98bf43f3fa127ce951a5
Samer Awajan <samer@simplify9.com>
2020-09-17 21:14:04 +0300

    ..

--- SW.InfolinkAdapters.Mappers.JsonToDelimited.UnitTests/UnitTest1.cs
+++ SW.InfolinkAdapters.Mappers.JsonToDelimited.UnitTests/UnitTest1.cs
@@ -15,9 +15,6 @@ namespace SW.InfolinkAdapters.Mappers.JsonToDelimited.UnitTests
             var handler = new Handler();
             Runner.MockRun(handler, new ServerlessOptions(),
                 new Dictionary<string, string>{ 
-                    { "BlobStorageAdaptor.ConnectionString", "DefaultEndpointsProtocol=https;AccountName=bcrm1;AccountKey=n+taOdDuZf9nJxp13qO/C/YF1tMH23ulnDrKIHzKZa+/W0kQztWFhNQs1K0Zvq+21YSVu/IQAtxi+Wzuju0TAg==;EndpointSuffix=core.windows.net" },
-                    {"BlobStorageAdaptor.ContainerName", "customers-out-dev"},
-                    {"BlobStorageAdaptor.FileExtension", "csv" }           
                 
                 });
             await handler.Handle(new XchangeFile("sss", "testfile.txt"));
