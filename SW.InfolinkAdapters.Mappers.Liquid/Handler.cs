fdd955b7b2764afe887a3107614f2e2c69d209b7
Samer Zughul <samerz@simplify9.com>
2020-10-07 14:32:20 +0300

    fixes

--- SW.InfolinkAdapters.Mappers.Liquid/Handler.cs
+++ SW.InfolinkAdapters.Mappers.Liquid/Handler.cs
@@ -13,21 +13,35 @@ namespace SW.InfolinkAdapters.Mappers.Liquid
         public Handler()
         {
             Runner.Expect(CommonProperties.DataTemplate);
-            Runner.Expect(CommonProperties.FileNameTemplate, false);
+            Runner.Expect(CommonProperties.FileNameTemplate, null);
         }
 
         public Task<XchangeFile> Handle(XchangeFile xchangeFile)
         {
+            
             var obj = JsonConvert.DeserializeObject<IDictionary<string, object>>(xchangeFile.Data, new DictionaryConverter());
             var jsonHash = Hash.FromDictionary(obj);
+
+            var fileNameTemplate = Runner.StartupValueOf(CommonProperties.FileNameTemplate);
+            string fileName = null;
+            if (fileNameTemplate != null)
+            {
+                var fileNameTemplateData = Runner.StartupValueOf(CommonProperties.FileNameTemplate);
+            
+                var parsedFileNameTemplate = Template.Parse(fileNameTemplateData);
+            
+                fileName = parsedFileNameTemplate.Render(jsonHash);
+            }
+            
             
             var templateData = Runner.StartupValueOf(CommonProperties.DataTemplate);
             
             var parsedTemplate = Template.Parse(templateData);
             
             var result = parsedTemplate.Render(jsonHash);
-
-            return Task.FromResult(new XchangeFile(result));
+            
+            
+            return Task.FromResult(new XchangeFile(result,fileName));
 
           
         }
