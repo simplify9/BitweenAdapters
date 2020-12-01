90ddc80a58050da5339c5fd60b7992bfb7819282
shaheen-s <shaheensarafa17@gmail.com>
2020-12-01 16:37:42 +0200

    UT and adapter

---
+++ SW.InfolinkAdapters.Handlers.Mailgun.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SW.InfolinkAdapters.Handlers.Mailgun.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- SW.InfolinkAdapters.Handlers.Mailgun/Handler.cs
+++ SW.InfolinkAdapters.Handlers.Mailgun/Handler.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SW.PrimitiveTypes;
@@ -18,20 +22,52 @@ namespace SW.InfolinkAdapters.Handlers.Mailgun
         public async Task<XchangeFile> Handle(XchangeFile xchangeFile)
         {
             IList<IEnumerable<byte>> attachmentsData = new List<IEnumerable<byte>>();
-            MailGunSendRequest rq = JsonConvert.DeserializeObject<MailGunSendRequest>(xchangeFile.Data);
+            MailGunSendRequest mailgunRequest = JsonConvert.DeserializeObject<MailGunSendRequest>(xchangeFile.Data);
+            
+            
+            HttpClient client = new HttpClient();
+            string key = Runner.StartupValueOf<string>("ApiKey");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", key );
+            
+            MultipartFormDataContent formContent = new MultipartFormDataContent();
+            
+            formContent.Add(new StringContent(mailgunRequest.To), "to");
+            formContent.Add(new StringContent(mailgunRequest.From), "from");
+            formContent.Add(new StringContent(mailgunRequest.Subject), "subject");
+            
+            
 
-            if (rq.Template != null)
+            if (mailgunRequest.Template != null)
             {
+                formContent.Add(new StringContent(mailgunRequest.Template), "template");
+                formContent.Add(new StringContent(JsonConvert.SerializeObject(mailgunRequest.TemplateVariables)), "h:X-Mailgun-Variables");
+                
                 
             }
-            else if (rq.Body != null)
+            else if (mailgunRequest.Body != null)
             {
                 
+                formContent.Add(new StringContent(mailgunRequest.Template), "body");
             }
             else
             {
-                
+                throw new Exception();
             }
+
+            if (mailgunRequest.AttachmentLocations != null)
+            {
+                foreach (string location in mailgunRequest.AttachmentLocations)
+                {
+                    Stream stream = await client.GetStreamAsync(location);
+                    formContent.Add(new StreamContent(stream), "attachment");
+                }
+            }
+
+            string mailgunEndpoint = Runner.StartupValueOf<string>("MailgunEndpoint");
+            
+            HttpResponseMessage message = await client.PostAsync(mailgunEndpoint, formContent);
+            
+            return new XchangeFile(await message.Content.ReadAsStringAsync());
             
         }
     }
