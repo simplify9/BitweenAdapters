240cc24de9da44c7aa5fd1ed16047732f5eb0f3f
ahmadabuhussen <ahmad@simplify9.com>
2023-01-09 11:15:14 +0300

    send email to multiple emails and add unit test

--- SW.Infolink.Adapters.Handlers.Notifier.Sendgrid/Handler.cs
+++ SW.Infolink.Adapters.Handlers.Notifier.Sendgrid/Handler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -11,7 +12,7 @@ using SW.Serverless.Sdk;
 
 namespace SW.Infolink.Adapters.Handlers.Notifier.Sendgrid
 {
-    public class Handler: IInfolinkHandler
+    public class Handler : IInfolinkHandler
     {
         public Handler()
         {
@@ -19,15 +20,14 @@ namespace SW.Infolink.Adapters.Handlers.Notifier.Sendgrid
             Runner.Expect(CommonProperties.From);
             Runner.Expect(CommonProperties.To);
             Runner.Expect(CommonProperties.Subject);
-
         }
-        
+
         public async Task<XchangeFile> Handle(XchangeFile xchangeFile)
         {
             var model = JsonConvert.DeserializeObject<NotificationModel>(xchangeFile.Data);
-            
-            var client = new SendGridClient( Runner.StartupValueOf<string>(CommonProperties.ApiKey));
-            
+
+            var client = new SendGridClient(Runner.StartupValueOf<string>(CommonProperties.ApiKey));
+
             var msg = new SendGridMessage()
             {
                 From = new EmailAddress(Runner.StartupValueOf<string>(CommonProperties.From)),
@@ -37,10 +37,21 @@ namespace SW.Infolink.Adapters.Handlers.Notifier.Sendgrid
                     ClickTracking = new ClickTracking { Enable = false }
                 },
             };
-            msg.AddTo(new EmailAddress(Runner.StartupValueOf<string>(CommonProperties.To)));
+
+            if (Runner.StartupValueOf<string>(CommonProperties.To).Contains(','))
+            {
+                var tos = Runner.StartupValueOf<string>(CommonProperties.To).Split(',')
+                    .Select(e => new EmailAddress(e)).ToList();
+
+                msg.AddTos(tos);
+            }
+            else
+            {
+                msg.AddTo(new EmailAddress(Runner.StartupValueOf<string>(CommonProperties.To)));
+            }
 
             var status = model.Success ? "Succeeded" : "Failed";
-            
+
             msg.HtmlContent = @$"<h1>Xchange Number {model.Id}</h1>
                <span><strong>ID</strong>{model.Id}</span>
                <span><strong>Status</strong>{status}</span>
@@ -49,10 +60,8 @@ namespace SW.Infolink.Adapters.Handlers.Notifier.Sendgrid
                 <span><strong>Bad Output</strong>{model.ResponseBad}</span>
                 <span><strong>Exception</strong>{model.Exception}</span>
             ";
-            
-            
-            
-            
+
+
             var response = await client.SendEmailAsync(msg);
 
             if (response.StatusCode < HttpStatusCode.BadRequest) return new XchangeFile(response.StatusCode.ToString());
