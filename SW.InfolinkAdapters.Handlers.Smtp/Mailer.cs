fd18427ba3e73eb27a72abfe268445c89f879fe0
waheeb :) <waheeb@simplify9.com>
2023-05-24 13:18:01 +0300

    smtp attachment

--- SW.InfolinkAdapters.Handlers.Smtp/Mailer.cs
+++ SW.InfolinkAdapters.Handlers.Smtp/Mailer.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
 
 namespace SW.InfolinkAdapters.Handlers.Smtp;
 
@@ -7,7 +9,7 @@ public static class Mailer
 {
     public static void SendEmail(string smtpHost, int smtpPort, string fromEmail, string fromPassword,
         string toEmail,
-        string subject, string body, bool isBodyHtml)
+        string subject, string body, bool isBodyHtml, string emailModelAttachmentName, string emailModelAttachmentBody)
     {
         using var mail = new MailMessage();
         mail.From = new MailAddress(fromEmail);
@@ -15,6 +17,13 @@ public static class Mailer
         mail.Subject = subject;
         mail.Body = body;
         mail.IsBodyHtml = isBodyHtml;
+        if (!string.IsNullOrEmpty(emailModelAttachmentName) && !string.IsNullOrEmpty(emailModelAttachmentBody))
+        {
+            var textBytes = Encoding.UTF8.GetBytes(emailModelAttachmentBody);
+            var memoryStream = new MemoryStream(textBytes);
+            var attachment = new Attachment(memoryStream, emailModelAttachmentName);
+            mail.Attachments.Add(attachment);
+        }
 
         using var smtp = new SmtpClient(smtpHost, smtpPort);
         smtp.Credentials = new NetworkCredential(fromEmail, fromPassword);
