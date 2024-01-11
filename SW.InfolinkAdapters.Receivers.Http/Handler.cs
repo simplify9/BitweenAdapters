62b9ca4d542cc01784998f753b43cccb12088595
omar <omar@simplify9.com>
2024-01-11 12:55:29 +0300

    Modified the receivers handler

--- SW.InfolinkAdapters.Receivers.Http/Handler.cs
+++ SW.InfolinkAdapters.Receivers.Http/Handler.cs
@@ -1,117 +1,129 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SW.PrimitiveTypes;
 using SW.Serverless.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SW.InfolinkAdapters.Receivers.Http
 {
-    public class Handler:IInfolinkReceiver
+  public class Handler : IInfolinkReceiver
+  {
+    public Handler()
     {
-        public Handler()
-        {
-            Runner.Expect(CommonProperties.AuthType, null);
-            Runner.Expect(CommonProperties.ApiKey, null);
-            Runner.Expect(CommonProperties.LoginUrl, null);
-            Runner.Expect(CommonProperties.Username, null);
-            Runner.Expect(CommonProperties.Password, null);
-            Runner.Expect(CommonProperties.Url);
-            Runner.Expect(CommonProperties.Headers, null);
-            Runner.Expect(CommonProperties.ContentType, "application/json");
-            Runner.Expect(CommonProperties.Verb, "get");
-        }
-        private HttpMethod HttpMethodFromString(string method)
-        {
-            switch (method.ToLower())
-            {
-                case "get":
-                    return HttpMethod.Get;
-                case "delete":
-                    return HttpMethod.Delete;
-                case "put":
-                    return HttpMethod.Put;
-                case "post":
-                default:
-                    return HttpMethod.Post;
-            }
-        }
-        
-        
-        
-        public async Task Initialize()
-        {
-            await Task.FromResult(new { });
-        }
-
-        public async Task Finalize()
-        {
-            await Task.FromResult(new { });
-        }
-
-        public async Task<IEnumerable<string>> ListFiles()
-        {
-            return await Task.FromResult(new[] {"1"});
-        }
-
-        public async Task<XchangeFile> GetFile(string fileId)
-        {
-            var options = new Options();
-
-            var client = new HttpClient();
-
-            if (options.AuthType == "ApiKey")
-            {
-                client.DefaultRequestHeaders.Add("ApiKey", options.ApiKey);
-            }
-            else if (options.AuthType == "Bearer")
-            {
-                client.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue("Bearer", options.LoginPassword);
-                
-            }
-            
-            HttpContent content;
-           
-            var uri = new Uri(options.Url);
-           
-
-            var request = new HttpRequestMessage
-            {
-                RequestUri = uri,
-                Method = HttpMethodFromString(options.Verb),
-            };
-
-            var headers = options.Headers?.Split(',').Select(h =>
-            {
-                var split = h.Split(':');
-                return new KeyValuePair<string, string>(split[0], split[1]);
-            });
-
-            if (headers != null)
-                foreach (var keyValuePair in headers)
-                    request.Headers.Add(keyValuePair.Key, keyValuePair.Value);
-
-            var response = await client.SendAsync(request);
-
-            if ((int)response.StatusCode >= 200 && (int)response.StatusCode < 500)
-            {
-                var resp = await response.Content.ReadAsStringAsync();
-
-                return (int)response.StatusCode < 400 ?
-                    new XchangeFile(resp) :
-                    new XchangeFile(resp, badData: true);
-            }
-
-            throw new Exception(response.StatusCode.ToString());
-        }
-
-        public async Task DeleteFile(string fileId)
-        {
-            await Task.FromResult(new { });
-        }
+      Runner.Expect(CommonProperties.AuthType,  null);
+      Runner.Expect(CommonProperties.ApiKey,  null);
+      Runner.Expect(CommonProperties.LoginUrl,  null);
+      Runner.Expect(CommonProperties.Username,  null);
+      Runner.Expect(CommonProperties.Password,  null);
+      Runner.Expect(CommonProperties.Url);
+      Runner.Expect(CommonProperties.Headers,  null);
+      Runner.Expect(CommonProperties.ContentType, "application/json");
+      Runner.Expect(CommonProperties.Verb, "get");
     }
+
+    private HttpMethod HttpMethodFromString(string method)
+    {
+      switch (method.ToLower())
+      {
+        case "get":
+          return HttpMethod.Get;
+        case "delete":
+          return HttpMethod.Delete;
+        case "put":
+          return HttpMethod.Put;
+        default:
+          return HttpMethod.Post;
+      }
+    }
+
+    public async Task Initialize()
+    {
+      var data = await Task.FromResult(new{  });
+    }
+
+    public async Task Finalize()
+    {
+      var data = await Task.FromResult(new{  });
+    }
+
+    public async Task<IEnumerable<string>> ListFiles()
+    {
+      string[] strArray = await Task.FromResult<string[]>(new string[1]
+      {
+        "1"
+      });
+      return strArray;
+    }
+
+    public async Task<XchangeFile> GetFile(string fileId)
+    {
+      Options options = new Options();
+      HttpClient client = new HttpClient();
+      if (options.AuthType == "ApiKey")
+        client.DefaultRequestHeaders.Add("ApiKey", options.ApiKey);
+      else if (options.AuthType == "Basic")
+      {
+        string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(options.LoginUsername + ":" + options.LoginPassword));
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+      }
+      else if (options.AuthType == "Bearer")
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", options.LoginPassword);
+      HttpContent content = (HttpContent) null;
+      if (!string.IsNullOrEmpty(Runner.StartupValueOf("DefaultRequest")))
+      {
+        string requestBody = Runner.StartupValueOf("DefaultRequest");
+        string str = options.ContentType.ToLower();
+        switch (str)
+        {
+          case "application/x-www-form-urlencoded":
+            content =  new FormUrlEncodedContent( JsonConvert.DeserializeObject<Dictionary<string, string>>(requestBody));
+            break;
+          case "application/json":
+            content =  new StringContent(requestBody, Encoding.UTF8, "application/json");
+            break;
+          default:
+            content =  new StringContent(requestBody, Encoding.UTF8, options.ContentType);
+            break;
+        }
+      }
+      Uri uri = new Uri(options.Url);
+      HttpRequestMessage request = new HttpRequestMessage()
+      {
+        RequestUri = uri,
+        Method = HttpMethodFromString(options.Verb),
+        Content = content
+      };
+      string headers1 = options.Headers;
+      IEnumerable<KeyValuePair<string, string>> headers = headers1 != null ? ( headers1.Split(',')).Select((Func<string, KeyValuePair<string, string>>) (h =>
+      {
+        string[] strArray = h.Split(':');
+        return new KeyValuePair<string, string>(strArray[0], strArray[1]);
+      })) :  null;
+      if (headers != null)
+      {
+        foreach (KeyValuePair<string, string> keyValuePair1 in headers)
+        {
+          KeyValuePair<string, string> keyValuePair = keyValuePair1;
+          request.Headers.Add(keyValuePair.Key, keyValuePair.Value);
+        }
+      }
+      HttpResponseMessage response = await client.SendAsync(request);
+      if (response.StatusCode < HttpStatusCode.OK || response.StatusCode >= HttpStatusCode.InternalServerError)
+        throw new Exception(response.StatusCode.ToString());
+      string resp = await response.Content.ReadAsStringAsync();
+      XchangeFile file = response.StatusCode < HttpStatusCode.BadRequest ? new XchangeFile(resp) : new XchangeFile(resp, badData: true);
+      return file;
+    }
+
+    public async Task DeleteFile(string fileId)
+    {
+      var data = await Task.FromResult(new{  });
+    }
+  }
 }
