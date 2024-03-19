0ab5c9ac3818b5072123a8beb4c9fc22f4f41920
omar <omar@simplify9.com>
2024-03-19 11:48:23 +0300

    Added login to http receiver

--- SW.InfolinkAdapters.Receivers.Http/OAuth2Model.cs
+++ SW.InfolinkAdapters.Receivers.Http/OAuth2Model.cs
@@ -4,3 +4,14 @@ public class OAuth2Response
 {
     public string access_token { get; set; }
 }
+public class UserLoginModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class LoginResponse
+{
+    public string Jwt { get; set; }
+    public string Refresh { get; set; }
+}
